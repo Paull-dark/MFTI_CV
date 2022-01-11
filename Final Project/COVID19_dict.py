56c2f2b84280b41adc6c7850ddfd8a678e90f725
Paull-dark <krivchenya.paul@gmail.com>
2022-01-11 17:41:29 +0800

    preparing for aug

--- Final Project/COVID19_dict.py
+++ Final Project/COVID19_dict.py
@@ -19,66 +19,69 @@ def build_COVID19_data_dicts(
         use_cache: bool = True,
         target_indices: Optional[np.ndarray] = None,
         debug: bool = False,
-        data_type: str='train'
+        data_type: str='train',
+        cache_mode: str='train'
 ):
-    cache_path = Path('.') / f"dataset_dicts_cache_{data_type}.pkl"
+    cache_path = Path('.') / f"dataset_dicts_cache_{cache_mode}.pkl"
     if not use_cache or not cache_path.exists():
         print('Creating the data ==> ...')
         df_meta = pd.read_csv(f'{DATA_ROOT_RESIZED}/img_sz_640/meta_sz_640.csv')
-        train_meta = df_meta[df_meta.split == 'train']
+        sub_meta = df_meta[df_meta.split == data_type]
         if debug:
-            train_meta = train_meta.iloc[:100] # for debuging
+            sub_meta = sub_meta.iloc[:100] # for debuging
 
         # load one image to get image size
-        image_id = train_meta.iloc[0,0]
+        image_id = sub_meta.iloc[0,0]
         image_path = f'{DATA_ROOT_RESIZED}/img_sz_640/train/{image_id}.jpg'
         image = cv2.imread(image_path)
         resized_height, resized_width, ch = image.shape
         print(f"image shape: {image.shape}")
 
         dataset_dicts = []
-        for index, train_meta_row in tqdm(train_meta.iterrows(), total=len(train_meta)):
+        for index, sub_meta_row in tqdm(sub_meta.iterrows(), total=len(sub_meta)):
             record = {}
-            image_id, height, width,s = train_meta_row.values
+            image_id, height, width,s = sub_meta_row.values
             filename = f'{DATA_ROOT_RESIZED}/img_sz_640/train/{image_id}.jpg'
             record["file_name"] = filename
             record["image_id"] = image_id
             record["height"] = resized_height
             record["width"] = resized_width
             objs = []
-
-            for index2, row in train_df.query('id==@image_id').iterrows():
-                #print(row)
-                #print(row["class"])
-                class_name = row["class"]
-                class_id = row["integer_label"]
-                if class_id == 2: #"no class or none"
-                    # Используем класс none  c bbox покрывающей всю площадь изобр.
-                    # bbox_resized = [0, 0, resized_width, resized_height]
-                    bbox_resized = [50,50,200,200]
-                    obj = {
-                        "bbox": bbox_resized,
-                        "bbox_mode": BoxMode.XYXY_ABS,
-                        "category_id": class_id,
-                    }
-                else:
-                    # bbox_original = [int(row["x_min"]), int(row["y_min"]), int(row["x_max"]), int(row["y_max"])]
-                    h_ratio = resized_height / height
-                    w_ratio = resized_width / width
-                    bbox_resized = [
-                        float(row["x_min"]) * w_ratio,
-                        float(row["y_min"]) * h_ratio,
-                        float(row["x_max"]) * w_ratio,
-                        float(row["y_max"]) * h_ratio,
-                    ]
-                    obj = {
-                        "bbox": bbox_resized,
-                        "bbox_mode": BoxMode.XYXY_ABS,
-                        "category_id": class_id,
-                    }
-                    objs.append(obj)
-            record['annotations'] = objs
-            dataset_dicts.append(record)
+            if data_type != 'train':
+                dataset_dicts.append(record)
+            else:
+                for index2, row in train_df.query('id==@image_id').iterrows():
+                    #print(row)
+                    #print(row["class"])
+                    class_name = row["class"]
+                    class_id = row["integer_label"]
+                    if class_id == 2: #"no class or none"
+                        # Используем класс none  c bbox покрывающей всю площадь изобр.
+                        # bbox_resized = [0, 0, resized_width, resized_height]
+                        bbox_resized = [50,50,200,200]
+                        obj = {
+                            "bbox": bbox_resized,
+                            "bbox_mode": BoxMode.XYXY_ABS,
+                            "category_id": class_id,
+                        }
+                    else:
+                        # bbox_original = [int(row["x_min"]), int(row["y_min"]), int(row["x_max"]), int(row["y_max"])]
+                        h_ratio = resized_height / height
+                        w_ratio = resized_width / width
+                        bbox_resized = [
+                            float(row["x_min"]) * w_ratio,
+                            float(row["y_min"]) * h_ratio,
+                            float(row["x_max"]) * w_ratio,
+                            float(row["y_max"]) * h_ratio,
+                        ]
+                        obj = {
+                            "bbox": bbox_resized,
+                            "bbox_mode": BoxMode.XYXY_ABS,
+                            "category_id": class_id,
+                        }
+                        objs.append(obj)
+                record['annotations'] = objs
+                dataset_dicts.append(record)
         with open(cache_path, mode='wb') as f:
             pickle.dump(dataset_dicts,f)
 
