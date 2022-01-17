10c2a71f9b8151cf641820df65ad51bbfba5f766
Paull-dark <krivchenya.paul@gmail.com>
2022-01-17 12:40:05 +0800

    final

--- Final Project/config.py
+++ Final Project/config.py
@@ -5,7 +5,6 @@ TRAIN_STUDY_CSV = './dataset/siim-covid19-detection/train_study_level.csv'
 TRAIN_IMAGE_CSV = './dataset/siim-covid19-detection/train_image_level.csv'
 DATA_ROOT = './dataset/siim-covid19-detection'
 DATA_ROOT_RESIZED = './dataset/resized_pics'
-CASCADE_OUT = './cascade_out'
 SEED = 42
 
 @dataclass
